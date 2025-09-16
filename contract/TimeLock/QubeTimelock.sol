c3cf433762f842c0bc2a8fc62509dde1e679a6b0
MUROKO <muroko@users.noreply.github.com>
2025-09-16 15:39:04 -0400

    edit: qubetimelock v1.1-dev

--- contract/TimeLock/QubeTimelock.sol
+++ contract/TimeLock/QubeTimelock.sol
@@ -3,14 +3,23 @@ pragma solidity ^0.8.20;
 
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
-/// See TimelockController.
+/// @title QubeTimelock - v1.1
+/// @title QubeTimelock
+/// @notice Custom timelock controller extending OpenZeppelin's implementation
 contract QubeTimelock is TimelockController {
     string public constant NAME = "QubeTimelock";
 
+    /**
+     * @dev Constructor
+     * @param minDelay Minimum delay for operations in seconds
+     * @param proposers List of addresses that can propose operations
+     * @param executors List of addresses that can execute operations
+     * @param admin Address with admin privileges
+     */
     constructor(
         uint256 minDelay,
         address[] memory proposers,
         address[] memory executors,
-		address target
-    ) TimelockController(minDelay, proposers, executors, target) {}
+        address admin
+    ) TimelockController(minDelay, proposers, executors, admin) {}
 }
